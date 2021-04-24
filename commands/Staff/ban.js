75aa88bd371ca2ab7d722e5a828e393acdebfd3e
simon <simon42400@gmail.com>
2021-04-24 11:29:37 +0200

    all

--- commands/Staff/ban.js
+++ commands/Staff/ban.js
@@ -1,6 +1,6 @@
-const { MessageEmbed } = require("discord.js")
+const { GuildMember, MessageEmbed } = require('discord.js');
 
-module.exports = async function run(message, args) {
+module.exports.run = async (client, message, args) => {
 
   message.channel.bulkDelete(1, true)   
     const user = message.mentions.members.first()
