347e1109b0c4d2da0d0406e785f02d3928d155c6
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-03 21:03:22 +0200

    Update ready.js

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -4,5 +4,7 @@ module.exports = client => {
 
 console.log(`Je suis près ! Mon préfix : ${PREFIX}`)
 console.log('Jopère actuellement sur le serveur : ${serveur}')
+   discordBot.user.setActivity('Vous observe :=');
+ discordBot.user.setStatus('idle');
 
 }
