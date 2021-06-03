5f10657993de701178e91ce90155cea89831d4a6
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-04 00:37:30 +0200

    Update message.js

--- events/messagesAuto/message.js
+++ events/messagesAuto/message.js
@@ -14,4 +14,8 @@ if (message.content === "Qui pour apex ?" && message.author.client) return messa
 if (message.content === "Salut" && message.author.client) return message.reply('Aurevoir')
 if (message.content === "Non" && message.author.client) return message.reply('Oui')
 if (message.content === "tg" && message.author.client) return message.reply('reverse')
+
+if (message.content === 'purge') {
+    client.destroy();
+}
 }
