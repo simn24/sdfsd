3199f25951fe87b6aeea30a87fc30eab25c2c5cc
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-03 21:21:30 +0200

    Update send.js

--- commands/Staff/send.js
+++ commands/Staff/send.js
@@ -20,7 +20,7 @@ message.channel.send(embed)
       description: 'Repondre pong quand on envoie ping', // Défini sa description [Plus tard pour le s!help]
       cooldown: 5, // Défini le cooldown de la commande (en secondes)
       usage: '', // Utilisation de la commande {Exemple : si la commandes est s!ban <@pseudo> mettre : <@pseudo>}
-      isUserAdmin: true, // Vérifie si l'utilisateur a visé a les permissions
-      permissions: false, // Vérifie si l'utilisateur a besoin des permissions pour utiliser la commande
+      isUserAdmin: false, // Vérifie si l'utilisateur a visé a les permissions
+      permissions: true, // Vérifie si l'utilisateur a besoin des permissions pour utiliser la commande
       args: false // Vérifie si la commande a besoin d'arguments
 }
