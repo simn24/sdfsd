2154aa202004c13ab88d293d9a79c0f95e1d8399
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-04 00:36:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -20,8 +20,6 @@ const loadCommands = (dir = "./commands/") => {
 };
 
 loadCommands();
-if (message.content === 'purge') {
-    client.destroy();
 }
 // =============================================================== \\
 
