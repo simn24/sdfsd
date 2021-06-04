41f543e4b23db3d69affddb0f686e30b5d936b81
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-04 20:32:16 +0200

    f

--- events/logs/ready.js
+++
@@ -1,9 +0,0 @@
-const { PREFIX } = require('../../config');
-
-module.exports = client => {
-
-console.log(`Je suis près ! Mon préfix : ${PREFIX}`)
-console.log('Jopère actuellement sur le serveur : ${serveur}')
-
-
-}
