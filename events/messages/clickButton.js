2d9080c2f33fb2ec71b80a89e3966cc6bf07ec55
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-05 18:38:27 +0200

    Update clickButton.js

--- events/messages/clickButton.js
+++ events/messages/clickButton.js
@@ -5,8 +5,14 @@ module.exports = async (client, button) => {
 
   if (button.id === 'acceptID') {
     let memberRole = button.message.guild.roles.cache.find((r) => r.name === "membre");
-    await button.clicker.user.roles.add(memberRole.id);
+    await button.clicker.member.roles.add(memberRole);
+    button.reply.send(`${button.clicker.user}, Vous avez accepté le réglement avec succès !`)
 
+
+
+ setTimeout(function() {
+      button.reply.delete()
+    }, 6000)
   }
 
 }
