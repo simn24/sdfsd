c85c77467718184b3a2bf29c0bd3103f75331fb0
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-06 18:29:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-
 module.exports = {
   PREFIX = 's!',
   SERVEUR = 'BK Bucks',
   DBCONNECTION = "mongodb+srv://simon:12345sim@cluster0.resct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   DEFAULTSETTINGS: {
-    prefix:"s!",
+    prefix: "s!",
     logChannel: "logs",
     welcomeMessage: "Welcome {{user}}",
   }
