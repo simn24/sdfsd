05a338e81b1b7a67111d1fdcc798ec3af113c4d1
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-06 17:28:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.TOKEN = ''
 exports.PREFIX = 's!'
 exports.SERVEUR = 'BK Bucks'
-exports.DBCONNECTION = "mongodb://localhost:27017/test";
+exports.DBCONNECTION = "mongodb://localhost:27017/localhost";
 
 // Channel des join message \\
 exports.LOGCHANNEL = '828945726099685384' , '784443819634065429'
