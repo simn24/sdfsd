fff2414f34dac5192d08b8bcaa6a7a343ea80e72
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-07 20:56:41 +0200

    Update ready.js

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -1,8 +1,8 @@
 const { SERVEUR } = require('../../config');
 
-module.exports = client => {
+module.exports = (client, settings) => {
 
-console.log(`Je suis près ! Mon préfix : ${client.config.PREFIX}`)
+console.log(`Je suis près ! Mon préfix : ${settings.prefix}`)
 console.log(`Jopère actuellement sur le serveur : ${SERVEUR}`)
 client.user.setPresence({ activity: { name: 'twitch.tv/simn24', type: 'WATCHING' }, statu: 'online' })
 
