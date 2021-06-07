fa0e6480a2f5fd626e596ecd8b1dd1e438e6c786
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-07 13:17:26 +0200

    Update config.js

--- commands/admin/config.js
+++ commands/admin/config.js
@@ -1,9 +1,35 @@
 module.exports.run = async (client, message, args, settings) => {
   const getSetting = args[0];
   const newSetting = args.slice(1).join(" ");
-  return message.reply("merci de mettre une clé qui existe (prefix, logChannel, welcomeMessage)");
-
+  if (getSetting != "prefix" || getSetting != "logChannel" || getSetting != "welcomeMessage") return message.reply("Merci de mettre une clé qui existe ! ");
   
+  switch(getSetting) {
+    case "prefix": {
+      if(newSetting) {
+        await client.updateGuild(message.guild, { prefix: newSetting});
+        return message.channel.send(`Prefix mis à jour: \`${settings.prefix}\`-> \`${newSetting}\``);
+      }
+      message.channel.send(`Prefix actuel:\`${settings.prefix}\``)
+      break;
+      }
+    
+    case "logChannel": {
+      if(newSetting) {
+        await client.updateGuild(message.guild, { logChannel: newSetting});
+        return message.channel.send(`logChannel mis à jour: \`${settings.logChannel}\`-> \`${newSetting}\``);
+      }
+      message.channel.send(`logChannel actuel:\`${settings.logChannel}\``)
+      break;
+    }  
+    case "welcomeMessage": {
+      if(newSetting) {
+        await client.updateGuild(message.guild, { welcomeMessage: newSetting});
+        return message.channel.send(`welcomeMessage mis à jour: \`${settings.welcomeMessage}\`-> \`${newSetting}\``);
+      }
+      message.channel.send(`welcomeMessage actuel:\`${settings.welcomeMessage}\``)
+      break;
+    }
+  } 
 };
 
 module.exports.help = {
