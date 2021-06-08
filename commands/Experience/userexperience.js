9b46cc4b2b3898ea8bf54cab81ef55c523b06624
simn24 <83095256+simn24@users.noreply.github.com>
2021-06-08 19:39:48 +0200

    Create userexperience.js

---
+++ commands/Experience/userexperience.js
@@ -0,0 +1,15 @@
+module.exports.run = (client, message, args, settings, dbUser) => {
+  message.reply(`Vous avez ${dbUser.experience} EXP!`)
+  }
+  
+  module.exports.help = {
+    name: 'uexp', // Défini le nom de la commande
+    aliases: ['uxp'], // Défini ces alias [Plus tard pour le s!help]
+    category: 'experience', // Défini sa catégorie [Plus tard pour le s!help]
+    description: 'renvoie l\'expérience de l\'utilisateur', // Défini sa description [Plus tard pour le s!help]
+    cooldown: 5, // Défini le cooldown de la commande (en secondes)
+    usage: '', // Utilisation de la commande {Exemple : si la commandes est s!ban <@pseudo> mettre : <@pseudo>}
+    isUserAdmin: false, // Vérifie si l'utilisateur a visé a les permissions
+    permissions: false, // Vérifie si l'utilisateur a besoin des permissions pour utiliser la commande
+    args: false // Vérifie si la commande a besoin d'arguments
+  }
